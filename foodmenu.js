53187c7be55ec5416797e489a45cff2949a4ec3a
sainath1510 <47551321+sainath1510@users.noreply.github.com>
2019-09-18 23:45:00 +0530

    Add files via upload

--- foodmenu.js
+++ foodmenu.js
@@ -1,15 +1,15 @@
 
-function decrement(){
-    var qty=parseInt(document.getElementById('quantity1').innerHTML);
+function decrement(id1){
+    var qty=parseInt(document.getElementById(id1).innerHTML);
     if(qty>0){
     qty-=1;
-    document.getElementById('quantity1').innerHTML=qty;
+    document.getElementById(id1).innerHTML=qty;
     }
 }
 
 function increment(id1){
-    var qty=parseInt(document.getElementById('quantity1').innerHTML);
+    var qty=parseInt(document.getElementById(id1).innerHTML);
     qty+=1;
-    document.getElementById('quantity1').innerHTML=qty;
+    document.getElementById(id1).innerHTML=qty;
 
 }
